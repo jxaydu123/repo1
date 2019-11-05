350b6b36298e6a3b04a765efda94d5e6093cedbb
dujiongjun227 <dujiongjun227@pingan.com.cn>
2019-11-06 00:56:06 +0800

    修改第五次

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
 		System.out.println("HelloWorld!");
 		System.out.println("HelloWorld!");
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
 
     }
 }
